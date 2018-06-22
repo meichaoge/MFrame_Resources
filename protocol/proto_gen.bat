938aafe2e94c7a8476d377cf1390c1e1dffb0319
梅超 <386586291@qq.com>
2018-06-22 18:34:02 +0800

    包含Protobuf

--- protocol/proto_gen.bat
+++ protocol/proto_gen.bat
@@ -1,7 +1,4 @@
-protogen.exe -i:net_packet.proto -o:../client/Assets/Script/Protocol/Packet.cs -ns:protocol
-protogen.exe -i:result_codes.proto -o:../client/Assets/Script/Protocol/PacketResultCode.cs -ns:protocol
-protogen.exe -i:friend.proto -o:../client/Assets/Script/Protocol/PacketFriend.cs -ns:protocol
-protogen.exe -i:shop.proto -o:../client/Assets/Script/Protocol/PacketShop.cs -ns:protocol
-protogen.exe -i:common_pb.proto -o:../client/Assets/Script/Protocol/PacketCommonPb.cs -ns:protocol
-protogen.exe -i:task.proto -o:../client/Assets/Script/Protocol/PacketTask.cs -ns:protocol
+
+protogen.exe -i:common_pb.proto -o:../Assets/Script/Protocol/common_pb.cs -ns:protocol
+protogen.exe -i:common_pb2.proto -o:../Assets/Script/Protocol/common_pb2.cs -ns:protoco_Test
 pause
--- protocol/proto_gen_pvp.bat
+++
@@ -1,3 +0,0 @@
-protogen.exe -i:pvp_packet.proto -o:../client/Assets/Script/Protocol/PVP_Packet.cs -ns:protocol
-protogen.exe -i:pvp_common.proto -o:../client/Assets/Script/Protocol/PVP_Common.cs -ns:protocol
-pause
